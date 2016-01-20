3152575e513503ac966fd2a3fa734e3fe793b42e
sean.mcgimpsey <sean.mcgimpsey@gmail.com>
2016-01-20 11:03:07 +0000

    User restrictions and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
-
+before_action :set_user, only: [:edit, :update, :show]
+before_action :require_same_user, only: [:edit, :update]
 def index
 @users = User.paginate(page: params[:page], per_page: 5)
 end
-
 def new
 @user = User.new
 end
-
 def create
 @user = User.new(user_params)
 if @user.save
@@ -17,13 +16,9 @@ else
 render 'new'
 end
 end
-
 def edit
-@user = User.find(params[:id])
 end
-
 def update
-@user = User.find(params[:id])
 if @user.update(user_params)
 flash[:success] = "Your account was updated successfully"
 redirect_to articles_path
@@ -31,14 +26,20 @@ else
 render 'edit'
 end
 end
-
 def show
-@user = User.find(params[:id])
 @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 end
-
 private
 def user_params
 params.require(:user).permit(:username, :email, :password)
+end
+def set_user
+@user = User.find(params[:id])
+end
+def require_same_user
+if current_user != @user
+flash[:danger] = "You can only edit your own account"
+redirect_to root_path
+end
 end
 end
